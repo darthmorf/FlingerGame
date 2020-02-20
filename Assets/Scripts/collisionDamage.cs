65fcc62f5974569ba8e2b41c671c0d4620787ca9
darthmorf <sampoirier@email.com>
2020-02-20 15:31:37 +0000

    DestrictibleGibs

--- Assets/Scripts/collisionDamage.cs
+++ Assets/Scripts/collisionDamage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class collisionDamage : MonoBehaviour
 {
-    private float health = 20f;
+    public float health = 20f;
     // Update is called once per frame
     void OnCollisionEnter2D(Collision2D collision)
     {
