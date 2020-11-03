b297b11741692e61949bd54cccc5309fc192efee
Sam <sampoirier@email.com>
2020-11-03 21:06:32 +0000

    Camera moving

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    private float speed = 0.1f;
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed;
+        }
+
+        transform.position = pos;
+    }
+}
