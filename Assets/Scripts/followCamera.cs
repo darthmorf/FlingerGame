600f94f2b81dd7f7636a951acf6458b7f9d36443
Sam <sampoirier@email.com>
2020-11-04 00:43:15 +0000

    bg for game

---
+++ Assets/Scripts/followCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{ // Update is called once per frame
+
+    [SerializeField] private Transform camera;
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = camera.position.x;
+        pos.y = camera.position.y;
+        transform.position = pos;
+    }
+}
