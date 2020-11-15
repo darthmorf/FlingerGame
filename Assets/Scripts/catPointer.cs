673a2369ca7ae3ebf2f18c2e450c046e5b543590
darthmorf <sampoirier@email.com>
2020-11-15 18:03:21 +0000

    UI element to point at cat

---
+++ Assets/Scripts/catPointer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catPointer : MonoBehaviour
+{
+    [SerializeField] private GameObject cat;
+   
+    void Update()
+    {
+        Vector3 targetLocation = Camera.main.WorldToScreenPoint(cat.transform.position);
+        Vector3 toTarget = targetLocation - transform.position;
+        Vector3 rotatedToTarget = Quaternion.Euler(0, 0, 360) * toTarget;
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, rotatedToTarget);
+
+        transform.rotation = targetRotation;
+    }
+}
